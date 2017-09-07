efefdb35720befa950cd2ec3c13b6d37d6bef54f
snehel <30468469+snehel@users.noreply.github.com>
2017-09-07 23:27:19 +0530

    Update Class 13 - Random_forest_Mica.R

--- Class 13 - Random_forest_Mica.R
+++ Class 13 - Random_forest_Mica.R
@@ -8,7 +8,7 @@
 getwd()
 
 #Change the working directory to point to the location where the data files are stored
-setwd('C:/Users/RAJNEESH VISHNU/Desktop/MICA2017/Data')
+setwd('C:/Users/lenovo/Desktop/Folders/Subjects/Term 4/AMMA')
 
 
 # relevant packages for random forest functions and input/output orerations respectively
